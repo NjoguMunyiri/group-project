49a6371f678f939dbcfa99faf1c4947c0659d3aa
abdirahim9 <abdirahimm8@gmail.com>
2017-05-16 19:38:21 +0300

    initial commit

---
+++ content.js
@@ -0,0 +1,26 @@
+<li><a href="#">Parents</a><div class="paro"><form class="form-group" action="index.html" method="post">
+
+
+ADMISSSION NUMBER:<br>
+ <input id="input1" type="text" name="" value="">
+</form>
+</div>
+</li>
+
+<script>
+
+$(document).ready(function() {
+$("#id form").submit(function(event) {
+var input1=$("input#input1").val();
+
+$(".input1").text(input1);
+if (input1===#x) {
+
+$(#output).show();
+}
+
+
+event.preventDefault();
+});
+});
+</script>
