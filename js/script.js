c4f939c39ce9da0c88604e30c82b10bbc15a1b9e
Abdiwawi <abdijamzilan@gmail.com>
2017-05-18 12:27:56 +0300

    add code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-var dob = $("#born").val();
+// var dob = $("#born").val();
+
+$(document).ready(function(event) {
+      $("#form1 form").submit(function() {
+        var firstname = $("input#firstname").val();
+        var middlename = $("input#middlename").val();
+        var surname= $("input#surname").val();
+        var age = $("input#age").val();
+        var gender = $("input#gender").val();
+        var born = $("input#born").val();
+        var father = $("input#father").val();
+        var mother =$("input#mother").val();
+        var occupation = $("input#occupation").val();
+        var contacts =$("input#contacts").val();
+        var address =$("input#contacs").val();
+
+        $(".firstname").text(firstnameInput);
+        $(".middlename").text(person2Input);
+        $(".surname").text(surnamelInput);
+        $(".age").text(ageInput);
+        $(".gender").text(genderInput);
+        $(".born").text(bornInput);
+        $(".father").text(fatherInput);
+        $(".mother").text(motherInput);
+        $(".occupation").text(occupationInput);
+        $(".contacts").text(contactsInput);
+        $(".address").text(addressInput);
+
+        $("form1").show();
+        $("#story").show();
+
+
+        event.preventDefault();
+      });
+    });
